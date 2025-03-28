7f939121c2fd6d030b95e99cff1e4556b2d68e2c
John Zambrano <134666133+JohnZambrano@users.noreply.github.com>
2025-03-28 22:01:23 +0100

    commit

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -12,7 +12,7 @@
         <loc>{{ url($url) }}</loc>
         <lastmod>2025-03-29T00:52:53Z</lastmod>
         <changefreq>{{ in_array($url, ['/', '/servicios/servicios', '/servicios/hoteles']) ? 'weekly' : 'monthly' }}</changefreq>
-        <priority>{{ $url === '/' ? '1.0' : (str_contains($url, 'servicios') ? '0.9' : '0.8' }}</priority>
+        <priority>{{ $url === '/' ? '1.0' : (str_contains($url, 'servicios') ? '0.9' : '0.8') }}</priority>
     </url>
 @endforeach
 </urlset>
