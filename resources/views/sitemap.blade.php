ec2d239cd9d86b1aa38587cdce3c3e2e469e8b0c
John Zambrano <134666133+JohnZambrano@users.noreply.github.com>
2025-03-28 21:00:35 +0100

    commit

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -1,38 +1,38 @@
-<?php echo '<?xml version="1.0" enconding="UTF-8"?>'; ?>
+@php echo '<?xml version="1.0" encoding="UTF-8"?>'; @endphp
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-<url>
+    <url>
         <loc>{{ url('/') }}</loc>
-        <lasrmond>2025-03-29T00:52:53Z</lastmod>
+        <lastmod>2025-03-29T00:52:53Z</lastmod>
         <changefreq>weekly</changefreq>
         <priority>1.0</priority>
     </url>
     <url>
         <loc>{{ url('/about/index') }}</loc>
-        <lastmond>2025-03-29T00:52:53Z</lastmod>
+        <lastmod>2025-03-29T00:52:53Z</lastmod>
         <changefreq>monthly</changefreq>
         <priority>0.8</priority>
     </url>
     <url>
         <loc>{{ url('/about/planes') }}</loc>
-        <lastmond>2025-03-29T00:52:53Z</lastmod>
+        <lastmod>2025-03-29T00:52:53Z</lastmod>
         <changefreq>monthly</changefreq>
         <priority>0.8</priority>
     </url>
     <url>
         <loc>{{ url('/about/municipio') }}</loc>
-        <lastmond>2025-03-29T00:52:53Z</lastmod>
+        <lastmod>2025-03-29T00:52:53Z</lastmod>
         <changefreq>monthly</changefreq>
         <priority>0.8</priority>
     </url>
     <url>
         <loc>{{ url('/servicios/servicios') }}</loc>
-        <lastmond>2025-03-29T00:52:53Z</lastmod>
+        <lastmod>2025-03-29T00:52:53Z</lastmod>
         <changefreq>weekly</changefreq>
         <priority>0.9</priority>
     </url>
     <url>
         <loc>{{ url('/servicios/hoteles') }}</loc>
-        <lastmond>2025-03-29T00:52:53Z</lastmod>
+        <lastmod>2025-03-29T00:52:53Z</lastmod>
         <changefreq>weekly</changefreq>
         <priority>0.9</priority>
     </url>
