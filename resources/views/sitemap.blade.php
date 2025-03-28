8a3b16bfbddfe8a087b97a4a6a19414657a28dfc
John Zambrano <134666133+JohnZambrano@users.noreply.github.com>
2025-03-28 21:14:57 +0100

    commit

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -1,5 +1,5 @@
 {!! '<?xml version="1.0" encoding="UTF-8"?>' !!}
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"> 
     <url>
         <loc>{{ url('/') }}</loc>
         <lastmod>2025-03-29T00:52:53Z</lastmod>
