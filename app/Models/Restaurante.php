e4e814826f0177304a9e978cd4d4df2a172bc754
John Zambrano <134666133+JohnZambrano@users.noreply.github.com>
2025-04-08 21:16:50 +0200

    commit modulo restaurante

--- app/Models/Restaurante.php
+++ app/Models/Restaurante.php
@@ -16,38 +16,5 @@ class Restaurante extends Model
     use HasFactory;
     protected $fillable = ['nombre', 'descripcion', 'imagen'];
 
-    // App\Models\Restaurante
-
-    public function getLikesAttribute() {
-        return $this->relationLoaded('likes') ? $this->getRelation('likes') : collect();
-    }
-    protected $withCount = ['likes'];
-
-  
-
-    public function likes()
-    {
-        return $this->belongsToMany(User::class, 'likes')
-                    ->withTimestamps();
-                    
-    }
-
-    // En tu modelo Restaurante.php
-    public function __call($method, $parameters) {
-        if ($method === 'likes') {
-            return $this->belongsToMany(User::class, 'likes')
-                    ->using(\Illuminate\Database\Eloquent\Relations\Pivot::class)
-                    ->withTimestamps();
-        }
-        return parent::__call($method, $parameters);
-    }
-
-    // Sobreescribe el método para evitar errores
-    public function getRelationValue($key) {
-        if ($key === 'likes' && !array_key_exists($key, $this->relations)) {
-            return $this->getRelationshipFromMethod($key);
-        }
-        return parent::getRelationValue($key);
-    }
-        
+   
  }
